be8377ee1c7fe537327686c6a86fa891de3f10aa
jubairk <jubairskv@gmail.com>
2025-01-21 01:08:59 +0530

    android setup for nativwWind tailwind css

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import './global.css';
+
+const App = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-3xl text-white">App</Text>
+    </View>
+  );
+};
+
+export default App;
